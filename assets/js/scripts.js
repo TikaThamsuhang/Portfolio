ac7a953f13eaefda5d4eac059bcdf67475aba90a
TikaThamsuhang <tikathamsuhang1@gmail.com>
2025-11-10 12:04:15 +0545

    made form changes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,6 @@
 const form = document.getElementById("my-form");
 const popup = document.getElementById("thank-you-popup");
 
-function showPopup() {
-  popup.classList.add('visible');
-  setTimeout(() => {
-    popup.classList.remove('visible');
-  }, 3000); // Hide after 3 seconds
-}
-
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
   
@@ -20,7 +13,7 @@ form.addEventListener("submit", async (event) => {
 
   if (response.ok) {
     form.reset();
-    showPopup();
+    popup.style.display = "block"; // show popup
   } else {
     alert("Something went wrong. Please try again.");
   }
